019686573d09ba5ffa4806ce708196b4abd380e6
hhcomp2-star <hhcomp2@gmail.com>
2026-07-15 22:37:32 -0400

    Cambio estético

--- modulos/version_app.py
+++ modulos/version_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 def mostrar_version_de_la_app():
-    st.caption("ExpreX Logística v1.7.8 • 🔒 Conexión Segura • ☁️ Railway Cloud Safe")
+    st.caption("ExpreX Logística v1.7.8 • 🔒 Conexión Segura • ☁️ Railway Cloud Safe • Persistencia Activada")
