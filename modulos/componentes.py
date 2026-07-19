32fb4b177f9e0cd4e175b29a6103915f123dbeb5
hhcomp2-star <hhcomp2@gmail.com>
2026-07-19 01:14:55 -0400

    Mejora estética en cabecera del formulario

--- modulos/componentes.py
+++ modulos/componentes.py
@@ -10,20 +10,21 @@ def mostrar_encabezado_exprex():
     ruta_logo = "favicon.ico"
 
     # Creamos dos columnas: una pequeña para el logo y una grande para el texto
-    col_logo, col_titulo = st.columns([1, 5])
+    col_logo, col_titulo = st.columns([1, 8])
 
     with col_logo:
         if os.path.exists(ruta_logo):
             # Un ancho de 60px para que actúe como un icono nítido
-            st.image(ruta_logo, width=60)
+            st.image(ruta_logo, width=65)
         else:
             # Colocamos un espacio vacío o un emoji de respaldo si el archivo no existe
             st.write("🚛")
 
     with col_titulo:
         # Colocamos un pequeño margen superior en HTML para alinear verticalmente el texto con el logo
-        st.markdown(
-            "<h2 style='margin-top: 25px; margin-bottom: 0px; line-height: 1;'>ExpreX Logística</h2>",
+        #st.markdown(
+            st.markdown("<h2 style='margin:0; padding:0; line-height:1.1;'>ExpreX Logística</h1>", unsafe_allow_html=True)
+            #"<h2 style='margin-top: 25px; margin-bottom: 0px; line-height: 1;'>ExpreX Logística</h2>",
             #"<h2 style='margin-top: 18px; margin-bottom: 0px;'>ExpreX Logística</h2>", 
-            unsafe_allow_html=True
-        )
+            #unsafe_allow_html=True
+        #)
